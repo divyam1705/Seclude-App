ace3bf0beab05ca85403cc72e33aa8e1a51862a3
Divyam Arora <89800407+divyam1705@users.noreply.github.com>
2023-01-11 02:25:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose=require("mongoose");
 const session=require("express-session");
 const passport=require("passport");
 const passportLocalMongoose=require("passport-local-mongoose");
-const findOrCreate=require("mongoose-findOrCreate")
+const findOrCreate=require("mongoose-findOrCreate");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy=require("passport-facebook");
 
