ee51d3688ea2286d04d04221641c126a871795fc
Divyam Arora <89800407+divyam1705@users.noreply.github.com>
2023-01-11 02:43:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://tame-teal-goose-garb.cyclic.app/auth/google/secrets"
     // ,userProfileUrl:"https://googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -63,7 +63,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+    callbackURL: "https://tame-teal-goose-garb.cyclic.app/auth/facebook/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
